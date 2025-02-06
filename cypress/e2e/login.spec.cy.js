fdaf55c6e304477c01e75de5d16eeeac9f4fedb6
fokeny <fabiocastro653@gmail.com>
2025-02-06 16:18:10 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -10,7 +10,7 @@ describe('Orange HRM Tests', () => {
      wrongCredentialAlert: "[role='alert']"
   }
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSeccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSeccess.password)
     cy.get(selectorsList.loginButton).click()
@@ -18,7 +18,7 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.dashBoardGrid)
   })
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
